d50dcb46c0c360cac4736298c5f87c8dabbc4f67
alex lee <w656957526@vip.qq.com>
2021-08-04 19:15:26 +0800

    feat：新增

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,41 @@
-import TelegramBot from "node-telegram-bot-api";
-import {createForm} from './utils'
-const token = "1917863854:AAH-HkOjh58cddWz_kuvoWqXF3wStlv2SzQ";
+import TelegramBot from 'node-telegram-bot-api'
+const token = '1917863854:AAH-HkOjh58cddWz_kuvoWqXF3wStlv2SzQ'
 const bot = new TelegramBot(token, {
   polling: true,
   // @ts-ignore
   request: {
-    proxy: "http://127.0.0.1:7890",
-  },
-});
+    proxy: 'http://127.0.0.1:7890'
+  }
+})
 
-bot.onText(/\/start/,(msg) => {
-  bot.sendMessage(msg.chat?.id as number,`选择业务`,{
-    parse_mode:'HTML',
+
+bot.onText(/\/confirm\_recharge_success\:/,(msg) => {
+  const userId = msg.text?.replace(/\/confirm_recharge_success:(\d+)/,'$1')
+  if(userId){
+    bot.sendMessage(userId,'充值已到账')
+  }
+})
+
+bot.onText(/\/confirm\_recharge_fail\:/,(msg) => {
+  const userId = msg.text?.replace(/\/confirm\_recharge_fail\:(\d+)/,'$1')
+  if(userId){
+    bot.sendMessage(userId,'充值未到账')
+  }
+})
+
+
+bot.onText(/\/querenchongzhi/,(msg) => {
+  bot.sendMessage(-476279416,'请确认充值',{
     reply_markup:{
       inline_keyboard:[
         [
           {
-            text:'充值',
-            callback_data:'pay'
+            text:'确认到账',
+            callback_data: `/confirm_recharge_success:${msg.from?.id}`
           },
           {
-            text:'转账',
-            callback_data:'trans'
-          },
-          {
-            text:'提现',
-            callback_data:'takeOut'
-          },
-          {
-            text:'发红包',
-            callback_data:'redPack'
+            text:'确认未到账',
+            callback_data:`/confirm_recharge_fail:${msg.from?.id}`
           }
         ]
       ]
@@ -37,101 +43,137 @@ bot.onText(/\/start/,(msg) => {
   })
 })
 
-bot.on(
-  "callback_query",
-  async (msg) => {
-   
-    if(msg.data === 'pay'){
-      
-       bot.sendMessage(msg.message?.chat.id as number,'充值',{
-        reply_markup:{
-          inline_keyboard:[
-            [
-              {
-                text:'人民币',
-                callback_data:'CNY_pay'
-              },
-              {
-                text:'USDT',
-                callback_data:'USDT_pay'
-              }
-            ]
+
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat?.id, `选择业务`, {
+    parse_mode: 'HTML',
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: '充值',
+            callback_data: '/pay'
+          },
+          {
+            text: '转账',
+            callback_data: '/trans'
+          },
+          {
+            text: '提现',
+            callback_data: '/takeOut'
+          },
+          {
+            text: '发红包',
+            callback_data: '/redPack'
+          }
+        ]
+      ]
+    }
+  })
+})
+
+bot.on('callback_query',msg => {
+  if(msg.data === '/pay'){
+    bot.sendMessage(msg.message?.chat.id as number, '充值', {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: '人民币',
+              callback_data: '/CNY_pay'
+            },
+            {
+              text: 'USDT',
+              callback_data: '/USDT_pay'
+            }
           ]
-        }
-      })
-    }
-    if(msg.data === 'CNY_pay'){
-      createForm(msg.message?.chat.id as number,{
-        count:{
-          question:'请输入充值金额'
-        },
-        number:{
-          question:'请输入转账数量'
-        }
-      },bot).then((res) => {
-        console.log(res)
-      })
-    }
-    // bot.editMessageReplyMarkup(
-    //   {
-    //   inline_keyboard: [
-    //     [
-    //       {
-    //         text: "2",
-    //         callback_data: "1",
-    //       },
-    //       {
-    //         text: "2",
-    //         callback_data: "2",
-    //       },
-    //       {
-    //         text: "3",
-    //         callback_data: "3",
-    //       },
-    //     ],
-    //     [
-    //       {
-    //         text: "4",
-    //         callback_data: "4",
-    //       },
-    //       {
-    //         text: "5",
-    //         callback_data: "5",
-    //       },
-    //       {
-    //         text: "6",
-    //         callback_data: "6",
-    //       },
-    //     ],
-    //     [
-    //       {
-    //         text: "7",
-    //         callback_data: "7",
-    //       },
-    //       {
-    //         text: "8",
-    //         callback_data: "8",
-    //       },
-    //       {
-    //         text: "9",
-    //         callback_data: "9",
-    //       },
-    //     ],
-    //   ],
-    // },
-    // {
-    //   chat_id:msg.message?.chat.id,
-    //   message_id:msg.message?.message_id
-    // }
-    // );
-    
-    
+        ]
+      }
+    })
   }
-);
+})
 
 
-bot.on("message", async (msg) => {
-   console.log('收到信息',msg);
+
+bot.onText(/\/CNY_pay/,(msg) => {
+  bot.sendMessage(msg.chat.id as number,'账号',{
+    reply_markup:{
+      inline_keyboard:[
+        [
+          {
+            text:'充值完成',
+            callback_data:'/querenchongzhi'
+          }
+        ]
+      ]
+    }
+  })
+})
+
+// bot.on('callback_query', async (msg) => {
+//   if (msg.data === 'pay') {
+   
+//   }
+//   if (msg.data === '/CNY_pay') {
+  
+//   }
+//   // bot.editMessageReplyMarkup(
+//   //   {
+//   //   inline_keyboard: [
+//   //     [
+//   //       {
+//   //         text: "2",
+//   //         callback_data: "1",
+//   //       },
+//   //       {
+//   //         text: "2",
+//   //         callback_data: "2",
+//   //       },
+//   //       {
+//   //         text: "3",
+//   //         callback_data: "3",
+//   //       },
+//   //     ],
+//   //     [
+//   //       {
+//   //         text: "4",
+//   //         callback_data: "4",
+//   //       },
+//   //       {
+//   //         text: "5",
+//   //         callback_data: "5",
+//   //       },
+//   //       {
+//   //         text: "6",
+//   //         callback_data: "6",
+//   //       },
+//   //     ],
+//   //     [
+//   //       {
+//   //         text: "7",
+//   //         callback_data: "7",
+//   //       },
+//   //       {
+//   //         text: "8",
+//   //         callback_data: "8",
+//   //       },
+//   //       {
+//   //         text: "9",
+//   //         callback_data: "9",
+//   //       },
+//   //     ],
+//   //   ],
+//   // },
+//   // {
+//   //   chat_id:msg.message?.chat.id,
+//   //   message_id:msg.message?.message_id
+//   // }
+//   // );
+// })
+
+bot.on('message', async (msg) => {
+  console.log('收到信息', msg)
   // const chatInfo = await bot.getChat(msg.chat.id)
   // console.log(chatInfo)
   // const a = bot.sendMessage(msg.chat.id,`选择业务`,{
@@ -240,6 +282,5 @@ bot.on("message", async (msg) => {
   //     ]
   //   }
   // })
-  //bot.sendMessage(msg.chat.id,msg?.text as string)
-});
-
+  // bot.sendMessage(msg.chat.id,msg?.text as string)
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,39 @@
 import TelegramBot from 'node-telegram-bot-api'
 
-
-
 interface form {
-  [key:string]:{
-    validate?:(value:string) => boolean
-    question:string
+  [key: string]: {
+    validate?: (value: string) => boolean
+    question: string
   }
 }
 
-type createForm = (chat_id:number | string,form:form,bot:TelegramBot) => Promise<{[key:string]:string}>
+type createForm = (chat_id: number | string, form: form, bot: TelegramBot) => Promise<{[key: string]: string}>
 
-export const createForm:createForm = async (chat_id,form,bot) => {
+export const createForm: createForm = async (chat_id, form, bot) => {
   const formValues = {}
-  for(let key in form){
-    const value = await runForm(chat_id,key,form[key],bot)
+  for (let key in form) {
+    const value = await runForm(chat_id, key, form[key], bot)
     formValues[key] = value
   }
   return formValues
 }
 
-const runForm =  (chat_id:number | string,key:string,formItem:form['key'],bot:TelegramBot) => {
-  return bot.sendMessage(chat_id,formItem.question,{
+const runForm = (chat_id: number | string, key: string, formItem: form['key'], bot: TelegramBot) => {
+  return bot.sendMessage(chat_id, formItem.question, {
     disable_web_page_preview: true,
-    reply_markup:{
-      force_reply:true
+    reply_markup: {
+      force_reply: true
     }
-  }).then((messgae:TelegramBot.Message) => {
-    return new Promise((resolve) =>{
-      const replyId = bot.onReplyToMessage(chat_id,messgae.message_id,(msg:TelegramBot.Message) => {
+  }).then((messgae: TelegramBot.Message) => {
+    return new Promise((resolve) => {
+      const replyId = bot.onReplyToMessage(chat_id, messgae.message_id, (msg: TelegramBot.Message) => {
         bot.removeReplyListener(replyId)
-        if(formItem.validate && formItem.validate(msg.text as string)){
+        if (formItem.validate && formItem.validate(msg.text as string)) {
           resolve(msg.text)
-        }else {
-         resolve(runForm(chat_id,key,formItem,bot))
+        } else {
+          resolve(runForm(chat_id, key, formItem, bot))
         }
       })
     })
   })
 }
-
-
