9928dd0daa0a67db0ddfa8ba0cfe97672d0ada3d
alex lee <w656957526@vip.qq.com>
2021-08-05 19:09:21 +0800

    feat: 增加

--- src/index.ts
+++ src/index.ts
@@ -1,286 +1,230 @@
-import TelegramBot from 'node-telegram-bot-api'
-const token = '1917863854:AAH-HkOjh58cddWz_kuvoWqXF3wStlv2SzQ'
-const bot = new TelegramBot(token, {
-  polling: true,
-  // @ts-ignore
-  request: {
-    proxy: 'http://127.0.0.1:7890'
-  }
+import { Composer, Context, Markup, Scenes, session, Telegraf } from "telegraf";
+import { SocksProxyAgent } from "socks-proxy-agent";
+import {validateAmount} from './utils'
+
+const token = "1917863854:AAH-HkOjh58cddWz_kuvoWqXF3wStlv2SzQ";
+if (token === undefined) {
+  throw new Error("BOT_TOKEN must be provided!");
+}
+
+interface MySession extends Scenes.WizardSession {
+  // will be available under `ctx.session.mySessionProp`
+  amount: number;
+  username: string;
+}
+
+
+interface MyContext extends Context {
+  // will be available under `ctx.myContextProp`
+  myContextProp: string;
+  // declare session type
+  session: MySession;
+  // declare scene type
+  scene: Scenes.SceneContextScene<MyContext, Scenes.WizardSessionData>;
+  // declare wizard type
+  wizard: Scenes.WizardContextWizard<MyContext>;
+}
+
+interface transContext extends Context {
+  // will be available under `ctx.myContextProp`
+  myContextProp: string;
+  // declare session type
+  session: MySession;
+  // declare scene type
+  scene: Scenes.SceneContextScene<MyContext, Scenes.WizardSessionData>;
+  // declare wizard type
+  wizard: Scenes.WizardContextWizard<MyContext>;
+}
+
+
+interface transState {
+  username?:string
+  amount?:number
+  type:'CNY' | 'USDT'
+}
+
+const stepHandler = new Composer<MyContext>();
+stepHandler.action("confirm_trans", async (ctx) => {
+  //转账
+  await ctx.reply('转账成功')
+  return ctx.scene.leave();
+});
+stepHandler.action('cancel_trans',async (ctx) => {
+  //取消
+  await ctx.reply('取消转账')
+  return ctx.scene.leave()
 })
 
-
-bot.onText(/\/confirm\_recharge_success\:/,(msg) => {
-  const userId = msg.text?.replace(/\/confirm_recharge_success:(\d+)/,'$1')
-  if(userId){
-    bot.sendMessage(userId,'充值已到账')
-  }
-})
-
-bot.onText(/\/confirm\_recharge_fail\:/,(msg) => {
-  const userId = msg.text?.replace(/\/confirm\_recharge_fail\:(\d+)/,'$1')
-  if(userId){
-    bot.sendMessage(userId,'充值未到账')
-  }
-})
-
-
-bot.onText(/\/querenchongzhi/,(msg) => {
-  bot.sendMessage(-476279416,'请确认充值',{
-    reply_markup:{
-      inline_keyboard:[
-        [
-          {
-            text:'确认到账',
-            callback_data: `/confirm_recharge_success:${msg.from?.id}`
-          },
-          {
-            text:'确认未到账',
-            callback_data:`/confirm_recharge_fail:${msg.from?.id}`
-          }
-        ]
-      ]
-    }
-  })
-})
+const transScene = new Scenes.WizardScene(
+  "trans",
+  async (ctx) => {
+    const state = ctx.scene.state as transState
+    await ctx.reply(
+      `确认转账信息,向${state.username}转账${state.amount}`,
+      Markup.inlineKeyboard([
+        Markup.button.callback("确认转账", "confirm_trans"),
+        Markup.button.callback("取消", "cancel_trans"),
+      ])
+    );
+    return ctx.wizard.next();
+  },
+  stepHandler
+);
+const stage = new Scenes.Stage<transContext>([transScene])
+const bot = new Telegraf<MyContext>(token, {
+  telegram: {
+    agent: new SocksProxyAgent("socks://127.0.0.1:7890"),
+  },
+});
 
 
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat?.id, `选择业务`, {
-    parse_mode: 'HTML',
+bot.hears(/\/alipay_fail\:\d+/, (ctx) => {
+  console.log("充值失败", ctx);
+});
+bot.hears(/\/alipay_success\:\d+/, (ctx) => {
+  console.log("充值成功", ctx);
+});
+
+bot.hears("confirm_alipay", (ctx) => {
+  bot.telegram.sendMessage(-476279416, "用户充值", {
     reply_markup: {
       inline_keyboard: [
         [
           {
-            text: '充值',
-            callback_data: '/pay'
+            text: "确认到账",
+            callback_data: `/alipay_success:${ctx.from.id}`,
           },
           {
-            text: '转账',
-            callback_data: '/trans'
+            text: "确认未到账",
+            callback_data: `/alipay_fail:${ctx.from.id}`,
           },
+        ],
+      ],
+    },
+  });
+});
+bot.hears("confirm_usdt", (ctx) => {
+  bot.telegram.sendMessage(-476279416, "usdt充值", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "确认到账", callback_data: `/usdt_success:${ctx.from.id}` },
           {
-            text: '提现',
-            callback_data: '/takeOut'
+            text: "确认未到账",
+            callback_data: `/usdt_fail:${ctx.from.id}`,
           },
+        ],
+      ],
+    },
+  });
+});
+bot.command("alipay", (ctx) => {
+  ctx.reply("支付邮箱xxxxxxx.com,请付款", {
+    reply_markup: {
+      inline_keyboard: [
+        [
           {
-            text: '发红包',
-            callback_data: '/redPack'
-          }
-        ]
-      ]
-    }
-  })
-})
+            text: "支付完成",
+            callback_data: `/confirm_alipay`,
+          },
+        ],
+      ],
+    },
+  });
+});
 
-bot.on('callback_query',msg => {
-  if(msg.data === '/pay'){
-    bot.sendMessage(msg.message?.chat.id as number, '充值', {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: '人民币',
-              callback_data: '/CNY_pay'
-            },
-            {
-              text: 'USDT',
-              callback_data: '/USDT_pay'
-            }
-          ]
-        ]
-      }
+bot.command("usdt", (ctx) => {
+  ctx.reply("充值地址xxxxxxx,请充值", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "充值完成",
+            callback_data: "/confirm_usdt",
+          },
+        ],
+      ],
+    },
+  });
+});
+
+bot.command("chongzhi", (ctx) => {
+  ctx.reply("选择充值渠道", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "支付宝",
+            callback_data: "/alipay",
+          },
+          {
+            text: "USDT-TRC20",
+            callback_data: "/usdt",
+          },
+        ],
+      ],
+    },
+  });
+});
+
+bot.command("start_a", (ctx) => {
+  ctx.reply("欢迎首页", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "充值",
+            callback_data: "/chongzhi",
+          },
+          {
+            text: "转账",
+            callback_data: "/zhuanzhang",
+          },
+          {
+            text: "提现",
+            callback_data: "/tixian",
+          },
+          {
+            text: "红包",
+            callback_data: "/hongbao",
+          },
+        ],
+      ],
+    },
+  });
+});
+
+bot.use(session())
+bot.use((ctx, next) => {
+  const now = new Date()
+  ctx.myContextProp = now.toString()
+  return next()
+})
+bot.use(stage.middleware())
+bot.launch();
+bot.command("zhuanzhang", (ctx) => {
+  ctx.scene.enter('trans')
+});
+
+bot.command(['rmb_zz','usdt_zz'],(ctx) => {
+  const {text} = ctx.message;
+  const rmbReg = new RegExp(/rmb\_zz\s+\@(\S+)\s+([0-9.]+)/)
+  const usdtReg = new RegExp(/usdt\_zz\s+\@(\S+)\s+([0-9.]+)/)
+  const isCNY = text.startsWith('rmb_zz') ? 'CNY' : 'USDT'
+  
+  const username = text.replace(isCNY ? rmbReg : usdtReg,'$1')
+  const amount = text.replace(isCNY ? rmbReg:usdtReg,'$2')
+  
+  if(username && amount && validateAmount(amount)){
+    ctx.scene.enter('trans',{
+      type:isCNY,
+      username,
+      amount:parseFloat(amount)
     })
+  }else {
+    ctx.reply('输入有误')
   }
 })
 
 
-
-bot.onText(/\/CNY_pay/,(msg) => {
-  bot.sendMessage(msg.chat.id as number,'账号',{
-    reply_markup:{
-      inline_keyboard:[
-        [
-          {
-            text:'充值完成',
-            callback_data:'/querenchongzhi'
-          }
-        ]
-      ]
-    }
-  })
-})
-
-// bot.on('callback_query', async (msg) => {
-//   if (msg.data === 'pay') {
-   
-//   }
-//   if (msg.data === '/CNY_pay') {
-  
-//   }
-//   // bot.editMessageReplyMarkup(
-//   //   {
-//   //   inline_keyboard: [
-//   //     [
-//   //       {
-//   //         text: "2",
-//   //         callback_data: "1",
-//   //       },
-//   //       {
-//   //         text: "2",
-//   //         callback_data: "2",
-//   //       },
-//   //       {
-//   //         text: "3",
-//   //         callback_data: "3",
-//   //       },
-//   //     ],
-//   //     [
-//   //       {
-//   //         text: "4",
-//   //         callback_data: "4",
-//   //       },
-//   //       {
-//   //         text: "5",
-//   //         callback_data: "5",
-//   //       },
-//   //       {
-//   //         text: "6",
-//   //         callback_data: "6",
-//   //       },
-//   //     ],
-//   //     [
-//   //       {
-//   //         text: "7",
-//   //         callback_data: "7",
-//   //       },
-//   //       {
-//   //         text: "8",
-//   //         callback_data: "8",
-//   //       },
-//   //       {
-//   //         text: "9",
-//   //         callback_data: "9",
-//   //       },
-//   //     ],
-//   //   ],
-//   // },
-//   // {
-//   //   chat_id:msg.message?.chat.id,
-//   //   message_id:msg.message?.message_id
-//   // }
-//   // );
-// })
-
-bot.on('message', async (msg) => {
-  console.log('收到信息', msg)
-  // const chatInfo = await bot.getChat(msg.chat.id)
-  // console.log(chatInfo)
-  // const a = bot.sendMessage(msg.chat.id,`选择业务`,{
-  //   parse_mode:'HTML',
-  //   reply_markup:{
-  //     inline_keyboard:[
-  //       [
-  //         {
-  //           text:'充值',
-  //           callback_data:'pay'
-  //         },
-  //         {
-  //           text:'转账',
-  //           callback_data:'trans'
-  //         },
-  //         {
-  //           text:'提现',
-  //           callback_data:'takeOut'
-  //         },
-  //         {
-  //           text:'发红包',
-  //           callback_data:'redPack'
-  //         }
-  //       ]
-  //     ]
-  //   }
-  // })
-  // console.log('a',a.then((res) => {console.log(res)}))
-  // bot.sendMessage(msg.chat.id,msg?.text as string,{
-  //   reply_to_message_id:msg.message_id
-  // })
-  // bot.sendChatAction(msg.chat.id, "typing");
-  // bot.sendMessage(msg.chat.id, "欢迎进入测试", {
-  //   reply_markup: {
-  //     inline_keyboard: [
-  //       [
-  //         {
-  //           text: "1",
-  //           callback_data: "1",
-  //         },
-  //         {
-  //           text: "2",
-  //           callback_data: "2",
-  //         },
-  //         {
-  //           text: "3",
-  //           callback_data: "3",
-  //         },
-  //       ],
-  //       [
-  //         {
-  //           text: "4",
-  //           callback_data: "4",
-  //         },
-  //         {
-  //           text: "5",
-  //           callback_data: "5",
-  //         },
-  //         {
-  //           text: "6",
-  //           callback_data: "6",
-  //         },
-  //       ],
-  //       [
-  //         {
-  //           text: "7",
-  //           callback_data: "7",
-  //         },
-  //         {
-  //           text: "8",
-  //           callback_data: "8",
-  //         },
-  //         {
-  //           text: "9",
-  //           callback_data: "9",
-  //         },
-  //       ],
-  //     ],
-  //   },
-  // });
-  // bot.sendMessage(msg.chat.id,'欢迎进入测试',{
-  //   reply_markup:{
-  //     keyboard:[
-  //       [
-  //         {
-  //           text:'第一排1'
-  //         },
-  //         {
-  //           text:'第一排2'
-  //         },
-  //         {
-  //           text:'第一排3'
-  //         }
-  //       ],
-  //       [
-  //         {
-  //           text:'第2排1'
-  //         },
-  //         {
-  //           text:'第2排2'
-  //         },
-  //         {
-  //           text:'第2排3'
-  //         }
-  //       ]
-  //     ]
-  //   }
-  // })
-  // bot.sendMessage(msg.chat.id,msg?.text as string)
-})
